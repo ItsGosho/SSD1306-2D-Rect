0564f0edf6a2df3208d7a60bb3e4b14ce87c95a8
itsgosho <itsgosho2@gmail.com>
2022-10-07 15:58:39 +0300

    up/down left/right moving

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@
 Adafruit_SSD1306 oledDisplay;
 
 #define OP_TL 0
-#define OP_CT 1
+#define OP_TC 1
 #define OP_TR 2
-#define OP_CR 3
+#define OP_RC 3
 #define OP_C 4
-#define OP_CL 5
+#define OP_LC 5
 #define OP_BL 6
-#define OP_CB 7
+#define OP_BC 7
 #define OP_BR 8
 
 struct TwoDPoint {
@@ -26,20 +26,19 @@ struct TwoDPoint {
 };
 
 struct TwoDRInnerPoint {
-    TwoDPoint topLeftCorner;
-    TwoDPoint centerTop;
-    TwoDPoint topRightCorner;
-    TwoDPoint centerRight;
-    TwoDPoint center;
-    TwoDPoint centerLeft;
+    TwoDPoint topLeft;
+    TwoDPoint topRight;
     TwoDPoint bottomLeft;
-    TwoDPoint centerBottom;
     TwoDPoint bottomRight;
 };
 
 class TwoDRObject {
 
 public:
+
+    TwoDRObject() {
+    }
+
     /*
      * @param width can be only odd number. 1, 3, 5, 7 ...
      * @param height can be only odd number. 1, 3, 5, 7 ...
@@ -52,27 +51,121 @@ public:
 
     void draw(Adafruit_SSD1306& ssd1306, const TwoDPoint& point, const uint8_t& relative) {
 
-       if (this->isDraw)
+        if (this->isDraw)
             return;
 
-        TwoDPoint drawPoint = this->calculateDrawPoint(point, relative);
+        TwoDPoint topLeft = this->getTopLeft(point, relative);
 
-        Serial.println(drawPoint.x);
-        Serial.println(drawPoint.y);
-        Serial.println("ZZZZZZZZZZZZZ>");
-
-        for (uint8_t y = drawPoint.y; y < (drawPoint.y + this->height); ++y) {
-            for (uint8_t x = drawPoint.x; x < (drawPoint.x + this->width); ++x) {
-                Serial.println(x);
-                Serial.println(y);
-                Serial.println("------>");
+        for (uint8_t y = topLeft.y; y < (topLeft.y + this->height); ++y) {
+            for (uint8_t x = topLeft.x; x < (topLeft.x + this->width); ++x) {
                 ssd1306.drawPixel(x, y, WHITE);
             }
         }
 
         ssd1306.display();
 
-        /*TODO: Don't forget to update the twoDPoint's inner points after drawing.*/
+        uint8_t bottomRightX = topLeft.x + (width - 1);
+        uint8_t bottomRightY = topLeft.y + (height - 1);
+        TwoDPoint bottomRight{bottomRightX, bottomRightY};
+
+        uint8_t bottomLeftX = topLeft.x;
+        uint8_t bottomLeftY = topLeft.y + (height - 1);
+        TwoDPoint bottomLeft{bottomLeftX, bottomLeftY};
+
+        uint8_t topRightX = topLeft.x + (width - 1);
+        uint8_t topRightY = topLeft.y;
+        TwoDPoint topRight{topRightX, topRightY};
+
+        this->innerPoint = TwoDRInnerPoint{topLeft, topRight, bottomLeft, bottomRight};
+        this->isDraw = true;
+    }
+
+    void moveLeft(Adafruit_SSD1306& ssd1306) {
+
+        if (!this->isDraw)
+            return;
+
+        for (uint8_t y = this->innerPoint.topLeft.y; y < (this->innerPoint.topLeft.y + this->height); ++y) {
+            ssd1306.drawPixel(this->innerPoint.topLeft.x - 1, y, WHITE);
+        }
+
+        ssd1306.display();
+        this->innerPoint.topLeft.x = this->innerPoint.topLeft.x - 1;
+        this->innerPoint.bottomLeft.x = this->innerPoint.bottomLeft.x - 1;
+
+        for (uint8_t y = this->innerPoint.topRight.y; y < (this->innerPoint.topRight.y + this->height); ++y) {
+            ssd1306.drawPixel(this->innerPoint.topRight.x, y, BLACK);
+        }
+
+        ssd1306.display();
+        this->innerPoint.topRight.x = this->innerPoint.topRight.x - 1;
+        this->innerPoint.bottomRight.x = this->innerPoint.bottomRight.x - 1;
+    }
+
+    void moveRight(Adafruit_SSD1306& ssd1306) {
+
+        if (!this->isDraw)
+            return;
+
+        for (uint8_t y = this->innerPoint.topRight.y; y < (this->innerPoint.topRight.y + this->height); ++y) {
+            ssd1306.drawPixel(this->innerPoint.topRight.x + 1, y, WHITE);
+        }
+
+        ssd1306.display();
+        this->innerPoint.topRight.x = this->innerPoint.topRight.x + 1;
+        this->innerPoint.bottomRight.x = this->innerPoint.bottomRight.x + 1;
+
+        for (uint8_t y = this->innerPoint.topLeft.y; y < (this->innerPoint.topLeft.y + this->height); ++y) {
+            ssd1306.drawPixel(this->innerPoint.topLeft.x, y, BLACK);
+        }
+
+        ssd1306.display();
+        this->innerPoint.topLeft.x = this->innerPoint.topLeft.x + 1;
+        this->innerPoint.bottomLeft.x = this->innerPoint.bottomLeft.x + 1;
+    }
+
+    void moveDown(Adafruit_SSD1306& ssd1306) {
+
+        if (!this->isDraw)
+            return;
+
+        for (uint8_t x = this->innerPoint.bottomLeft.x; x < (this->innerPoint.bottomLeft.x + this->width); ++x) {
+            ssd1306.drawPixel(x, this->innerPoint.bottomLeft.y + 1, WHITE);
+        }
+
+        ssd1306.display();
+        this->innerPoint.bottomLeft.y = this->innerPoint.bottomLeft.y + 1;
+        this->innerPoint.bottomRight.y = this->innerPoint.bottomRight.y + 1;
+
+        for (uint8_t x = this->innerPoint.topLeft.x; x < (this->innerPoint.topLeft.x + this->width); ++x) {
+            ssd1306.drawPixel(x, this->innerPoint.topLeft.y, BLACK);
+        }
+
+        ssd1306.display();
+        this->innerPoint.topLeft.y = this->innerPoint.topLeft.y + 1;
+        this->innerPoint.topRight.y = this->innerPoint.topRight.y + 1;
+    }
+
+    void moveUp(Adafruit_SSD1306& ssd1306) {
+
+        if (!this->isDraw)
+            return;
+
+        for (uint8_t x = this->innerPoint.topLeft.x; x < (this->innerPoint.topLeft.x + this->width); ++x) {
+            ssd1306.drawPixel(x, this->innerPoint.topLeft.y - 1, WHITE);
+        }
+
+        ssd1306.display();
+        this->innerPoint.topLeft.y = this->innerPoint.topLeft.y - 1;
+        this->innerPoint.topRight.y = this->innerPoint.topRight.y - 1;
+
+        for (uint8_t x = this->innerPoint.bottomLeft.x; x < (this->innerPoint.bottomLeft.x + this->width); ++x) {
+            ssd1306.drawPixel(x, this->innerPoint.bottomLeft.y, BLACK);
+        }
+
+        ssd1306.display();
+        this->innerPoint.bottomLeft.y = this->innerPoint.bottomLeft.y - 1;
+        this->innerPoint.bottomRight.y = this->innerPoint.bottomRight.y - 1;
     }
 
 private:
@@ -136,32 +229,35 @@ private:
         return {startingX, startingY};
     }
 
-    TwoDPoint calculateDrawPoint(const TwoDPoint& point, const uint8_t& relative) {
+    /**
+     * Get where the top left corner of the object is if given part of it is at given position.
+     */
+    TwoDPoint getTopLeft(const TwoDPoint& point, const uint8_t& relative) {
 
         switch (relative) {
 
             case OP_TL:
                 return this->calculateDrawPointTL(point);
 
-            case OP_CT:
+            case OP_TC:
                 return this->calculateDrawPointCT(point);
 
             case OP_TR:
                 return this->calculateDrawPointTR(point);
 
-            case OP_CR:
+            case OP_RC:
                 return this->calculateDrawPointCR(point);
 
             case OP_C:
                 return this->calculateDrawPointC(point);
 
-            case OP_CL:
+            case OP_LC:
                 return this->calculateDrawPointCL(point);
 
             case OP_BL:
                 return this->calculateDrawPointBL(point);
 
-            case OP_CB:
+            case OP_BC:
                 return this->calculateDrawPointCB(point);
 
             case OP_BR:
@@ -173,6 +269,11 @@ private:
     }
 };
 
+TwoDRObject twoDrObject;
+
+/*
+ * TODO: Следващото нещо, което трябва да направя е да сложа пример за това рисуване спрямо част от обекта в README-то :)
+ * */
 void setup() {
     Serial.begin(9600);
 
@@ -184,16 +285,21 @@ void setup() {
     delay(2000);
     oledDisplay.clearDisplay();
 
-    TwoDRObject twoDrObject = TwoDRObject(25, 25);
+    twoDrObject = TwoDRObject(10, 10);
 
     TwoDPoint twoDPoint = TwoDPoint{(OLED_WIDTH / 2) - 1, (OLED_HEIGHT / 2) - 1};
 
     twoDrObject.draw(oledDisplay, twoDPoint, OP_C);
+    delay(2000);
+
 
     /*oledDisplay.writePixel(0,0, WHITE);
     oledDisplay.display();*/
 }
 
 void loop() {
+
+    twoDrObject.moveUp(oledDisplay);
+    delay(500);
 
 }
